feb770229c829324a85eb43e7b69c8d64cdb0fdc
Raith Medina Villegas <rmedinav@unsa.edu.pe>
2022-03-16 11:20:31 -0500

    Se modifica Servidor

--- src/main/java/Servidor.java
+++ src/main/java/Servidor.java
@@ -17,7 +17,7 @@ public class Servidor {
         
         
         try {
-            ServerSocket server = new ServerSocket(5000);
+            ServerSocket server = new ServerSocket(50000);
             Socket sc;
             
             System.out.println("Servidor iniciado");
